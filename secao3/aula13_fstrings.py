f2a0b3a2aa5662b6ad9cd7fcc5dc4df674703cc3
JunioResende <JunioResende>
2023-03-06 12:58:01 -0300

    fix imc

--- secao3/aula13_fstrings.py
+++ secao3/aula13_fstrings.py
@@ -5,3 +5,5 @@ altura = float(input("Qual a sua altura em metros? "))
 imc = float((peso/(altura**2)))
 
 print(f"{nome} tem {idade} anos, pesa {peso:.2f}kg, mede{altura: .2f}m e seu IMC e de {imc: .2f}")
+# f de formatacao .2f significa duas casas apos a virgula
+
