e2b60171548b0a8c455f28dd338c101c3f2f759d
JunioResende <JunioResende>
2023-03-06 08:01:27 -0300

    aula6

---
+++ secao3/aula6_IntroducaoAosTiposdeDados_NumerosIntFloat.py
@@ -0,0 +1,15 @@
+#inteiros int
+print(-1)
+print(22)
+print(0)
+#decimais float
+print(1.2)#No python se usa ponto "." para separar decimais. A virgula e usada para separacao de argumentos
+
+#para saber o tipo de um dado basta usar a funcao type.
+num1 = 25
+num2 = 3.2
+texto = "Junio"
+
+print(type(num1))
+print(type(num2))
+print(type(texto))
