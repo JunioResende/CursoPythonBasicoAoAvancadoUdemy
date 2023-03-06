216c95312b074c36a344c98a7a95d55a6a523585
JunioResende <JunioResende>
2023-03-06 07:47:30 -0300

    aula5

---
+++ secao3/aula5_IntroducaoAosTiposdeDados_String.py
@@ -0,0 +1,17 @@
+# Strings sao testos que ficam dentro de aspas duplas ou simples
+# str -> string -> texto
+# Aspas Simples
+print('Junio Resende')
+#Aspas duplas
+print("Junio Resende")
+
+#Nao util
+#Escape
+print("Junio \"Resende\"")# Utilizando escape "\" para colocar caracteres
+#r
+print(r"Junio \"Resende\"")# o r mostra tudo que foi digitado entre as aspas
+
+# Aspas Simples
+print('Junio "Resende"')#se quiser colocar aspas na string basta inverter as aspas que delimitam a string
+#Aspas duplas
+print("Junio Resende")
