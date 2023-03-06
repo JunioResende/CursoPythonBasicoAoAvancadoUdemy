19fa2fa8c93f1138aaff8377c898e1491d7619f3
JunioResende <JunioResende>
2023-03-06 08:08:31 -0300

    aula7

---
+++ secao3/aula7_IntroducaoAosTiposdeDados_Boleanos.py
@@ -0,0 +1,9 @@
+# True e ou False
+
+num1 = 10
+num2 = 100
+
+print(num1 == num2) # nao sao iguais False
+print(num1 != num2) # sao diferentes True
+print(type(num1 != num2))#Imprime o tipo
+print(type(num1 == num2))
