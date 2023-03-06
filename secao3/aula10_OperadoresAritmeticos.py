361e57c3af46be5cb985e2b6572378da566e5731
JunioResende <JunioResende>
2023-03-06 09:10:14 -0300

    aula10

---
+++ secao3/aula10_OperadoresAritmeticos.py
@@ -0,0 +1,31 @@
+num1 = int(input("Digite um numero"))
+num2 = int(input("Digite outro numero"))
+
+print("adicao")
+print(num1 + num2)
+print("***********")
+
+print("subtracao")
+print(num1 - num2)
+print("***********")
+
+print("multiplicacao")
+print(num1 * num2)
+print("***********")
+
+print("divisao")
+print(num1 / num2)
+print("***********")
+
+print("divisao inteira")
+print(num1 // num2)
+print("***********")
+
+print("resto da divisao 'Modulo'")
+print(num1 % num2)
+print("***********")
+
+print("exponenciacao")
+print(num1 ** num2)
+print("***********")
+
