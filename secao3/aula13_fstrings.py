69f8eee6b06a2d021bf3a11297bbac386ed74a51
JunioResende <JunioResende>
2023-03-06 11:26:40 -0300

    fstrings

---
+++ secao3/aula13_fstrings.py
@@ -0,0 +1,7 @@
+nome = str(input("Qual o seu nome? "))
+idade = int(input("Qual a sua idade? "))
+peso = float(input("Qual o seu peso? "))
+altura = float(input("Qual a sua altura em metros? "))
+imc = float((peso/(altura**2)))
+
+print(f"{nome} tem {idade} anos, pesa {peso:.2f}kg, mede{altura: .2f}m e seu IMC e de {imc: .2f}")
