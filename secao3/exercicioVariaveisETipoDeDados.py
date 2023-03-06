7d2c8c0ee16f4715fcf6cccf6981f850a47dfda0
JunioResende <JunioResende>
2023-03-06 08:45:44 -0300

    challange solved

---
+++ secao3/exercicioVariaveisETipoDeDados.py
@@ -0,0 +1,13 @@
+nome = "Junio"
+sobrenome = "Resende"
+idade = 32
+ano_nascimento = 2023 - idade
+maior_de_idade = idade >= 18
+altura_m = 1.88
+
+print("Nome: ", nome, type(nome))
+print("Sobrenome: ", sobrenome, type(sobrenome))
+print("Idade: ", idade, type(idade))
+print("Ano de Nasimento: ", ano_nascimento, type(ano_nascimento))
+print("E maior de idade? ", maior_de_idade, type(maior_de_idade))
+print("Altura em metros: ", altura_m, type(altura_m))
